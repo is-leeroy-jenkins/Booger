10a42cac639666d05fa8360a2bb35f5e5e32c267
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-08-04 18:01:07 -0400

    updates;

--- UI/Windows/Chat/LiveChatUserControl.xaml.cs
+++ UI/Windows/Chat/LiveChatUserControl.xaml.cs
@@ -42,12 +42,12 @@
 namespace Booger
 {
     using System;
+    using System.Collections.Specialized;
+    using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
     using System.Windows.Media;
-    using System.Collections.Specialized;
-    using System.Diagnostics.CodeAnalysis;
     using ModernWpf.Controls;
 
     /// <inheritdoc />

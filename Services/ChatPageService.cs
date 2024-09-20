21b084d71f2be24e4f9166c05462054ab1398cc0
is-leeroy-jenkins <terry.eppler@gmail.com>
2024-09-19 20:12:42 -0400

    update readme;

--- Services/ChatPageService.cs
+++ Services/ChatPageService.cs
@@ -6,23 +6,45 @@ namespace Booger
     using System.Collections.Generic;
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class ChatPageService
     {
+        /// <summary>
+        /// The pages
+        /// </summary>
         private Dictionary<Guid, ChatPage> _pages =
             new Dictionary<Guid, ChatPage>();
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="ChatPageService"/> class.
+        /// </summary>
+        /// <param name="services">The services.</param>
         public ChatPageService( IServiceProvider services)
         {
             Services = services;
         }
 
+        /// <summary>
+        /// Gets the services.
+        /// </summary>
+        /// <value>
+        /// The services.
+        /// </value>
         public IServiceProvider Services { get; }
 
+        /// <summary>
+        /// Gets the page.
+        /// </summary>
+        /// <param name="sessionId">The session identifier.</param>
+        /// <returns></returns>
         public ChatPage GetPage(Guid sessionId)
         {
-            if (!_pages.TryGetValue(sessionId, out ChatPage _chatPage))
+            if (!_pages.TryGetValue( sessionId, out ChatPage _chatPage ) )
             {
-                using (var _scope = Services.CreateScope())
+                using ( var _scope = Services.CreateScope( ) )
                 {
                     _chatPage = (ChatPage)_scope.ServiceProvider.GetRequiredService( typeof( ChatPage ) );
                     _chatPage.InitSession(sessionId);
@@ -33,6 +55,11 @@ namespace Booger
             return _chatPage;
         }
 
+        /// <summary>
+        /// Removes the page.
+        /// </summary>
+        /// <param name="sessionId">The session identifier.</param>
+        /// <returns></returns>
         public bool RemovePage(Guid sessionId)
         {
             return _pages.Remove(sessionId);
--- Services/PageService.cs
+++ Services/PageService.cs
@@ -2,18 +2,38 @@
 namespace Booger
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    [ SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" ) ]
     public class PageService
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageService"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
         public PageService( IServiceProvider serviceProvider)
         {
             ServiceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Gets the service provider.
+        /// </summary>
+        /// <value>
+        /// The service provider.
+        /// </value>
         public IServiceProvider ServiceProvider { get; }
 
+        /// <summary>
+        /// Gets the page.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
         public FrameworkElement GetPage( Type type )
         {
             return ServiceProvider.GetService( type ) as FrameworkElement;
